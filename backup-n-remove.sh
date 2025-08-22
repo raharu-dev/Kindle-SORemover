408c7d7ee4bebc9ff92f8365e4829f5fe50804d5
raharu-dev <adamek.wola@gmail.com>
2025-08-22 13:19:04 +0200

    backup-n-remove changes and further testing

--- backup-n-remove.sh
+++ backup-n-remove.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 BACKUP_PATH="/mnt/us/BACKUP"
 
-echo BACKUP directory is: $BACKUP_PATH
+echo -BACKUP & REMOVE-
+echo Backup directory is: $BACKUP_PATH
 
 if [ ! -d "$BACKUP_PATH" ]; then
   mkdir "$BACKUP_PATH"
@@ -15,33 +16,32 @@ if [ ! -d "$BACKUP_PATH" ]; then
 fi
 
 # BACKUP and REMOVAL
+# .assets
 if [ -d "/mnt/us/system/.assets" ]; then
-    #cp -r /mnt/us/system/.assets $BACKUP_PATH/.assets
-    #rm -rf /mnt/us/system/.assets
-    :
+    cp -r /mnt/us/system/.assets $BACKUP_PATH/.assets
+    rm -rf /mnt/us/system/.assets
 else
   echo "No .assets directory found"
 fi
-if [ -d "/var/local/adunits/" ]; then
-    #cp -r /var/local/adunits/ $BACKUP_PATH/adunits/
-    #rm -rf /var/local/adunits/
-    :
+# /var/local files
+cd /var/local/
+if [ -d "adunits/" ]; then
+    cp -r adunits/ $BACKUP_PATH/adunits/
+    rm -rf adunits/
 else
   echo "No adunits directory found"
 fi
-if [ -d "/var/local/merchant/" ]; then
-    #cp -r /var/local/merchant/ $BACKUP_PATH/merchant/
-    #rm -rf /var/local/merchant/
-    :
+if [ -d "merchant/" ]; then
+    cp -r merchant/ $BACKUP_PATH/merchant/
+    rm -rf merchant/
 else
   echo "No merchant directory found"
 fi
-if [ -f "/var/local/appreg.db" ]; then
-    #cp /var/local/appreg.db $BACKUP_PATH/appreg.db
-    #sqlite3 appreg.db "delete from properties where handlerid='dcc' and name='adunit.viewable'"
-    #sqlite3 appreg.db "delete from properties where handlerid='dcc' and name='dtcp_pref_ShowScreensaverPref'"
-    #sqlite3 appreg.db "delete from properties where handlerid='dcc' and name='dtcp_pref_ShowBannerPref'"
-    :
+if [ -f "appreg.db" ]; then
+    cp appreg.db $BACKUP_PATH/appreg.db
+    sqlite3 /var/local/appreg.db "delete from properties where handlerid='dcc' and name='adunit.viewable'"
+    sqlite3 /var/local/appreg.db "delete from properties where handlerid='dcc' and name='dtcp_pref_ShowScreensaverPref'"
+    sqlite3 /var/local/appreg.db "delete from properties where handlerid='dcc' and name='dtcp_pref_ShowBannerPref'"
 else
   echo "No appreg.db file found"
 fi
