61f66b95e395fe5821b7db26551cb71811bcecf1
raharu-dev <adamek.wola@gmail.com>
2025-08-22 14:49:57 +0200

    File counter fixed

--- restore.sh
+++ restore.sh
@@ -18,7 +18,7 @@ else
   exit 1
 fi
 
-FILE_COUNT=$(find "$BACKUP_PATH" -type f | wc -l)
+FILE_COUNT=$(find "$BACKUP_PATH" -mindepth 1 \( -type f -o -type d \) | wc -l)
 FILE_COUNTER=0
 
 if [ "$FILE_COUNT" -eq 0 ]; then
@@ -28,7 +28,7 @@ fi
 
 # .assets
 if [ -d "$BACKUP_PATH/.assets" ]; then
-  cp -r "$BACKUP_PATH/.assets"/* /mnt/us/system/
+    cp -r "$BACKUP_PATH/.assets"/* /mnt/us/system/
     if [ -d "/mnt/us/system/.assets" ]; then
         echo "Restored .assets directory"
         FILE_COUNTER=$((FILE_COUNTER + 1))
@@ -38,36 +38,36 @@ if [ -d "$BACKUP_PATH/.assets" ]; then
 fi
 # adunits/
 if [ -d "$BACKUP_PATH/adunits" ]; then
-  cp -r "$BACKUP_PATH/adunits"/* /var/local/
-  if [ -d "/var/local/adunits" ]; then
-      echo "Restored adunits directory"
-      FILE_COUNTER=$((FILE_COUNTER + 1))
-  else
-      echo "Failed to restore adunits directory"
-  fi
+    cp -r "$BACKUP_PATH/adunits"/* /var/local/
+    if [ -d "/var/local/adunits" ]; then
+        echo "Restored adunits directory"
+        FILE_COUNTER=$((FILE_COUNTER + 1))
+    else
+        echo "Failed to restore adunits directory"
+    fi
 fi
 # merchant/
 if [ -d "$BACKUP_PATH/merchant" ]; then
-  cp -r "$BACKUP_PATH/merchant"/* /var/local/
-  if [ -d "/var/local/merchant" ]; then
-      echo "Restored merchant directory"
-      FILE_COUNTER=$((FILE_COUNTER + 1))
-  else
-      echo "Failed to restore merchant directory"
-  fi
+    cp -r "$BACKUP_PATH/merchant"/* /var/local/
+    if [ -d "/var/local/merchant" ]; then
+        echo "Restored merchant directory"
+        FILE_COUNTER=$((FILE_COUNTER + 1))
+    else
+        echo "Failed to restore merchant directory"
+    fi
 fi
 # appreg.db
 if [ -f "$BACKUP_PATH/appreg.db" ]; then
-  cp "$BACKUP_PATH/appreg.db" /var/local/
-  if [ -f "/var/local/appreg.db" ]; then
-      echo "Restored appreg.db"
-      FILE_COUNTER=$((FILE_COUNTER + 1))
-  else
-      echo "Failed to restore appreg.db"
-  fi
+    cp "$BACKUP_PATH/appreg.db" /var/local/
+    if [ -f "/var/local/appreg.db" ]; then
+        echo "Restored appreg.db"
+        FILE_COUNTER=$((FILE_COUNTER + 1))
+    else
+        echo "Failed to restore appreg.db"
+    fi
 fi
 
-echo "Restored $FILE_COUNT out of $FILE_COUNTER."
+echo "Restored $FILE_COUNTER out of $FILE_COUNT."
 if [ "$FILE_COUNT" -eq "$FILE_COUNTER" ]; then
   echo "All files restored successfully."
 else
